c42c5eeb0c2dea08c99b853b7361c7d487689f3f
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-09-13 18:17:44 +0000

    ...

--- Projects/portal/script.js
+++ Projects/portal/script.js
@@ -1,5 +1,5 @@
 // List of allowed IP addresses
-const allowedIPs = ['10.104.102.138', '987.654.321.0']; // Replace with actual IP addresses
+const allowedIPs = ['10.104.102.138', '136.228.205.245']; // Replace with actual IP addresses
 
 // Function to encode Unicode to base64
 function encodeUnicode(str) {
@@ -28,6 +28,7 @@ getUserIP(function(ip) {
         // Remove blur and display content if IP address is allowed
         document.getElementById('content').classList.remove('blur');
     } else {
+        alert(`Error 403: Forbidden. Your IP address is ${ip}. Please contact the administrator to get whitelisted.`);
         while (true) {
             alert('Error 403: Forbidden.');
         }
