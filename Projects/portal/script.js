f449065d9067ecb658f665a65c0057bb51e11140
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-09-16 19:30:07 +0000

    Updated panic key

--- Projects/portal/script.js
+++ Projects/portal/script.js
@@ -76,6 +76,5 @@ getUserIP(function(ip) {
 document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && event.shiftKey && event.key === 'X') {
         document.getElementById('content').classList.add('blur');
-        displayAccessDeniedMessage(ip);
     }
 });
