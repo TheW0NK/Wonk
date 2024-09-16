60fc40cf68d27bd44298e64e25f2a78645a2960b
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-09-16 19:27:12 +0000

    Updated script

--- Projects/portal/script.js
+++ Projects/portal/script.js
@@ -76,5 +76,6 @@ getUserIP(function(ip) {
 document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && event.shiftKey && event.key === 'X') {
         document.getElementById('content').classList.add('blur');
+        displayAccessDeniedMessage(ip);
     }
 });
