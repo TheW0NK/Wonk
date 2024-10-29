5bcd63ce99e93f29963af400f467246b3b40f733
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-10-29 19:06:09 +0000

    login.js

--- api/login.js
+++ api/login.js
@@ -14,6 +14,7 @@ const validCredentials = [
     { username: 'RSNOW', password: process.env.RSNOW_PASSWORD },
     { username: 'OB', password: process.env.OB_PASSWORD },
     { username: 'Langston' , password: process.env.LANGSTON_PASSWORD },
+    { username: 'Monkey', password: process.env.MONKEY_PASSWORD },
 ];
 
 app.post('/api/login', (req, res) => {
